7933084a7f656caaac26dcd90b80ce6774d0b981
Pshypher <bshote@getcarbon.co>
2025-04-10 16:46:52 +0100

    master/Migrate to the latest gradle and android gradle plugins
    (cherry picked from commit 396b4ac182e11ea257ef509f3a9a4a439bdf328b)

--- unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
+++ unit_testing_fundamentals/src/test/java/com/techyourchance/unittestingfundamentals/exercise1/NegativeNumberValidatorTest.java
@@ -1,5 +1,36 @@
 package com.techyourchance.unittestingfundamentals.exercise1;
 
+import static org.hamcrest.core.Is.is;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class NegativeNumberValidatorTest {
+
+    NegativeNumberValidator SUT;
+
+    @Before
+    public void setup() {
+        SUT = new NegativeNumberValidator();
+    }
+
+    @Test
+    public void test1() {
+        boolean actual = SUT.isNegative(-1);
+        Assert.assertThat(actual, is(true));
+    }
+
+    @Test
+    public void test2() {
+        boolean actual = SUT.isNegative(0);
+        Assert.assertThat(actual, is(true));
+    }
+
+    @Test
+    public void test3() {
+        boolean actual = SUT.isNegative(1);
+        Assert.assertThat(actual, is(false));
+    }
 
 }
